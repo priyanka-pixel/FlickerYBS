17785685f16c075f19ebd84490f9065478b678ac
Priyanka Singh <59287277+priyanka-pixel@users.noreply.github.com>
2023-07-25 11:49:44 +0100

    updated

--- app/src/main/java/com/priyanka/flickerybs/MainActivity.kt
+++ app/src/main/java/com/priyanka/flickerybs/MainActivity.kt
@@ -3,13 +3,23 @@ package com.priyanka.flickerybs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
+import com.priyanka.flickerybs.core.components.navigation.BottomNav
+import com.priyanka.flickerybs.core.components.navigation.NavGraph
+import com.priyanka.flickerybs.presentation.favorite_pics.FavoritePhotoViewModel
+import com.priyanka.flickerybs.presentation.search.SearchPhotoViewModel
 import com.priyanka.flickerybs.ui.theme.FlickerYBSTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,10 +34,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    Photos()
 
                 }
             }
         }
     }
 }
+@Composable
+fun Photos() {
+    val navController = rememberNavController()
+    val   favoritePhotoViewModel: FavoritePhotoViewModel = hiltViewModel()
+    val   photoListingViewModel: SearchPhotoViewModel = hiltViewModel()
+
+    Scaffold(
+        bottomBar = { BottomNav(navController = navController) },
+        content = { padding -> Column(modifier = Modifier.padding(padding)){
+            NavGraph(navController = navController,
+                favoritePhotoViewModel=favoritePhotoViewModel,
+                photoListingViewModel=photoListingViewModel
+            )
+        } },
+        backgroundColor = MaterialTheme.colors.surface,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
 
