05bf302314cfad221abf4b59a8bf92a3b83b05c9
Priyanka Singh <59287277+priyanka-pixel@users.noreply.github.com>
2023-07-26 18:06:16 +0100

    Update RemoteDatasource.kt

--- app/src/main/java/com/priyanka/flickerybs/data/remote/datasource/RemoteDatasource.kt
+++ app/src/main/java/com/priyanka/flickerybs/data/remote/datasource/RemoteDatasource.kt
@@ -1,7 +1,1 @@
-package com.priyanka.flickerybs.data.remote.datasource
 
-import com.priyanka.flickerybs.data.remote.dto.FlickerResponse
-
-interface RemoteDatasource {
-    suspend fun getPhotoListingFromApi(): FlickerResponse
-}
