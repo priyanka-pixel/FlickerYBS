2a5a39922c3c7174d85ca74154ba2084e662c477
Priyanka Singh <59287277+priyanka-pixel@users.noreply.github.com>
2023-07-26 18:06:05 +0100

    Update RemoteDataSourceImpl.kt

--- app/src/main/java/com/priyanka/flickerybs/data/remote/datasource/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/priyanka/flickerybs/data/remote/datasource/RemoteDataSourceImpl.kt
@@ -1,12 +1,1 @@
-package com.priyanka.flickerybs.data.remote.datasource
 
-import com.priyanka.flickerybs.data.remote.apiservice.ApiService
-import com.priyanka.flickerybs.data.remote.dto.FlickerResponse
-import javax.inject.Inject
-
-class RemoteDataSourceImpl @Inject constructor(
-    private val api: ApiService
-): RemoteDatasource
-{
-    override suspend fun getPhotoListingFromApi(): FlickerResponse =api.getRecentPhotos()
-}
